5ec6d3c6fbd07b821d8da6fd839707607a5d7496
walter <gzyiot2017@163.com>
2017-11-30 14:19:27 +0800

    OpenWrt-SDK-NHSS.ILQ-ar71xx-generic_gcc-4.8-linaro_uClibc-1.0.14.Linux-x86_64

---
+++ build_dir/target-mips_34kc_uClibc-1.0.14/linux-ar71xx_generic/linux-3.18.29/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 12 */
+#define UTS_MACHINE "mips"
+#define UTS_VERSION "#12 Thu Nov 9 09:56:45 CST 2017"
+#define LINUX_COMPILE_BY "z"
+#define LINUX_COMPILE_HOST "zlf"
+#define LINUX_COMPILER "gcc version 4.8.3 (OpenWrt/Linaro GCC 4.8-2014.04 unknown) "
---
+++ staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-1.0.14/lib/gcc/mips-openwrt-linux-uclibc/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/z/shuncom_gatewayQca4531/build_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-1.0.14/gcc-linaro-4.8-2014.04/configure --with-bugurl=https://dev.openwrt.org/ --with-pkgversion='OpenWrt/Linaro GCC 4.8-2014.04 unknown' --prefix=/home/z/shuncom_gatewayQca4531/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-1.0.14 --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=mips-openwrt-linux-uclibc --with-gnu-ld --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-multilib --disable-nls --with-host-libstdcxx=-lstdc++ --with-float=soft --with-gmp=/home/z/shuncom_gatewayQca4531/staging_dir/host --with-mpfr=/home/z/shuncom_gatewayQca4531/staging_dir/host --with-mpc=/home/z/shuncom_gatewayQca4531/staging_dir/host --disable-decimal-float --with-mips-plt --disable-libssp --disable-__cxa_atexit --with-headers=/home/z/shuncom_gatewayQca4531/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-1.0.14/include --disable-libsanitizer --enable-languages=c,c++ --enable-shared --enable-threads --with-slibdir=/home/z/shuncom_gatewayQca4531/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-1.0.14/lib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" }, { "llsc", "llsc" }, { "mips-plt", "plt" } };
